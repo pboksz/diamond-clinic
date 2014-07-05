c4eeb768dc6dfc116585de6fea1268ce767e8bfe
Phillip Boksz <pboksz@gmail.com>
2014-07-05 17:05:20 +0200

    Create new rails application

---
+++ config/application.rb
@@ -0,0 +1,34 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(:default, Rails.env)
+
+require 'a9n'
+A9n.root = File.expand_path('../..', __FILE__)
+A9n.load
+
+module DiamondClinic
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    config.i18n.enforce_available_locales = true
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    #config.i18n.default_locale = :pl
+
+    # for creating rspec specs instead of test on rail generate
+    config.generators.test_framework :rspec, :fixture => true
+
+    # for automatically creating factory girl fixtures
+    config.generators.fixture_replacement :factory_girl, :dir => 'spec/factories'
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DiamondClinic::Application.config.secret_key_base = '1fbfcaf7306418403cca028c48c167a5cfcbd4575531ec6b423a88bfbf35b371191d02fe40964e4a26f3d948e88467ac609d4103dd7c0221d0b8b63ff3e62fce'
