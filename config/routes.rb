4ccc6eb893e72e019242a7b0a0f28faf5341681e
Phillip Boksz <pboksz@gmail.com>
2014-08-05 22:33:29 +0200

    Add about us page with google map

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 DiamondClinic::Application.routes.draw do
   scope '(:locale)', :locale => /(en|pl)/ do
+    get '/about', :to => 'home#about_us'
     root 'home#index'
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -5,4 +5,9 @@ describe HomeController do
     before { get :index }
     it { expect(response).to render_template :index }
   end
+
+  describe 'GET #about_us' do
+    before { get :about_us }
+    it { expect(response).to render_template :about_us }
+  end
 end
