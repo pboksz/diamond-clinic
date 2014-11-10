d06704d6281e35904c92b045e24cea99ceb83fca
Phillip Boksz <phillip@lunarlogic.io>
2014-11-10 14:12:33 +0100

    Fix rendering doctor photo

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -2,9 +2,8 @@ class DoctorsController < ApplicationController
   def photo
     @doctor = Doctor.find(params[:id])
 
-    if @doctor.photo? && stale?(:etag => @doctor.photo.read, :last_modified => @doctor.updated_at.utc, :public => true)
+    if @doctor.photo?
       send_data @doctor.photo.read, :type => @doctor.photo.file.content_type, :disposition => 'inline'
-      expires_in 0, :public => true
     else
       render :nothing => true
     end
