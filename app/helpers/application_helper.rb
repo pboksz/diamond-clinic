0a686f7086a94436ad4616c75b612864307e5bf1
Phillip Boksz <pboksz@gmail.com>
2021-04-06 12:39:39 +0100

    Add parentheses in application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     max_length = options[:max_length] || 400
 
     if text.length > max_length
-      content_tag :div, truncated(text, max_length) + expanded(text), class: :readmore
+      content_tag(:div, truncated(text, max_length) + expanded(text), class: :readmore)
     else
       simple_format(text)
     end
@@ -20,14 +20,14 @@ module ApplicationHelper
   end
 
   def toggleable_tag(content, options = {})
-    content_tag :div, content, class: :toggleable, style: options[:style]
+    content_tag(:div, content, class: :toggleable, style: options[:style])
   end
 
   def text_tag(text)
-    content_tag :div, text
+    content_tag(:div, text)
   end
 
   def link_tag(type)
-    content_tag :a, I18n.t("views.readmore.#{type}"), class: "toggle #{type}"
+    content_tag(:a, I18n.t("views.readmore.#{type}"), class: "toggle #{type}")
   end
 end
